14645a59a8b6b55c0314348f69f21167abc598b9
Andrey Khomutov <homutovan@live.com>
2020-08-05 21:12:05 +0700

    fix bugs

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,14 @@
+import Character from './domain.js';
+
 class Game {
-    start() {
-      console.log('game started');
-    }
+  start() {
+    console.log('game started');
   }
-  
-  class GameSavingData {
-  }
-  
-  function readGameSaving() {
-  }
-  
-  function writeGameSaving() {
-  }
+}
+export class GameSavingData {
+}
+export function readGameSaving() {
+}
+export function writeGameSaving() {
+}
+export default Game;
